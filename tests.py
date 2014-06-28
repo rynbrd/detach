ebf4b8d5e56e17aff1ead1b6cb1cca2c4b1e80d2
Ryan Bourgeois <bluedragonx@gmail.com>
2014-06-28 15:32:59 -0700

    Add call() test and remove stdout and stdin from Detach() test calls.

--- tests.py
+++ tests.py
@@ -50,7 +50,7 @@ class TestDetach(unittest.TestCase):
         """Detach()"""
         try:
             want = deque()
-            with detach.Detach(sys.stdout, sys.stderr, sys.stdin) as d:
+            with detach.Detach(None, sys.stderr, None) as d:
                 if d.pid:
                     want.append(d.pid)
                 else:
@@ -65,7 +65,7 @@ class TestDetach(unittest.TestCase):
     def test_daemonize(self):
         """Detach(daemonize=True)"""
         try:
-            with detach.Detach(sys.stdout, sys.stderr, sys.stdin, daemonize=True):
+            with detach.Detach(None, sys.stderr, None, daemonize=True) as d:
                 pass
             raise Exception("parent did not exist")
         except SystemExit as e:
@@ -76,7 +76,7 @@ class TestDetach(unittest.TestCase):
         """Detach(close_fds=True)"""
         try:
             fd = tempfile.NamedTemporaryFile(delete=False)
-            with detach.Detach(sys.stdout, sys.stderr, sys.stdin, close_fds=True) as d:
+            with detach.Detach(None, sys.stderr, None, close_fds=True) as d:
                 if d.pid:
                     fd.close()
                 else:
@@ -89,11 +89,28 @@ class TestDetach(unittest.TestCase):
         """Detach(close_fds=True, exclude_fds=[fd])"""
         try:
             fd = tempfile.NamedTemporaryFile(delete=False)
-            with detach.Detach(sys.stdout, sys.stderr, sys.stdin,
-                               close_fds=True, exclude_fds=[fd]) as d:
+            with detach.Detach(None, sys.stderr, None, close_fds=True, exclude_fds=[fd]) as d:
                 if d.pid:
                     fd.close()
                 else:
                     fd.close()
+        except SystemExit as e:
+            self.assertEqual(e.code, 0)
+
+
+class TestCall(unittest.TestCase):
+    """Test `call` function."""
+
+    @parentonly
+    def test_call(self):
+        """call()"""
+        try:
+            fd = tempfile.NamedTemporaryFile(delete=False)
+            want_pid = detach.call(['bash', '-c', 'echo "$$"'], stdout=fd)
+            time.sleep(0.5)
+            fd.seek(0)
+            have_pid = int(fd.read().strip())
+            self.assertEqual(have_pid, want_pid)
+            fd.close()
         except SystemExit as e:
             self.assertEqual(e.code, 0)
